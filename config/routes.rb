a84a868ad32069b48b0fdef02a92945a8ed42d29
Tong <theerapong@bananacoding.com>
2019-01-17 20:11:58 +0700

    create rails admin

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'homes/index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	root 'homes#index'
+  resources :articles do
+  	resources :comments
+  end
+end
